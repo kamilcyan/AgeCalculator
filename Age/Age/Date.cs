f1cb0e11cb5aeee2f946d2446c8cf81c0912d5d3
kamilcyan <kamil.lucjanek@wp.pl>
2020-02-17 20:41:12 +0100

    działa

---
+++ Age/Age/Date.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Age
+{
+    public class Date
+    {
+        public DateTime BirthDate { get; set; }
+        public DateTime CurrentDate { get; set; }
+        public double numberOfSeconds { get; set; }
+
+        public Date(DateTime birt, DateTime now)
+        {
+            this.BirthDate = birt;
+            this.CurrentDate = now;
+        }
+
+        public double DateCounter()
+        {
+            numberOfSeconds = (CurrentDate - BirthDate).TotalSeconds;
+            return numberOfSeconds;
+        }
+    }
+}
---
+++ Age/Age/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Age
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dpick_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime date = dpick.SelectedDate.Value;
+            DateTime current = DateTime.Now;
+            Date dateFromClass = new Date(date, current);
+
+            double seconds = dateFromClass.DateCounter();
+            double Hours = dateFromClass.DateCounter()/3600;
+            double Days = dateFromClass.DateCounter()/86400;
+
+            SecondText.Text = seconds.ToString();
+            HoursText.Text = Hours.ToString();
+            DaysText.Text = Days.ToString();
+        }
+    }
+}
